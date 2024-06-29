bd4e6ee624be4ea001b7fd8644aa523a51dc5eb7
SYED <clarckck01122@gmail.com>
2024-06-29 04:44:15 -0700

    login commit

---
+++ src/signup.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const signup =() => {
+    return(
+        <div className="signup-container">
+            <form className="signup-form">
+                <h2>sign up</h2>
+                <label htmlFor="email">
+                    Email:
+                    <input type="text" />
+                 </label>
+                
+                </form></div>
+    )
+}
+
+export default signup
